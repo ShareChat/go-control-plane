84b6a3ee48b117cafcd4ce68cdfdd1ee8d5ce266
krhitesh7 <hiteshrawat@sharechat.co>
2024-10-15 15:41:56 +0530

    update

--- pkg/cache/v3/simple.go
+++ pkg/cache/v3/simple.go
@@ -956,10 +956,7 @@ func (cache *snapshotCache) CreateDeltaWatch(request *DeltaRequest, state stream
 			cache.log.Errorf("failed to respond with delta response: %s", err)
 		}
 
-		if snapshot.GetResourcesAndTTL(request.GetTypeUrl()) != nil {
-			delayedResponse = len(snapshot.GetResourcesAndTTL(request.GetTypeUrl())) == 0
-		}
-		delayedResponse = response == nil
+		delayedResponse = (response == nil) || (snapshot.GetResourcesAndTTL(request.GetTypeUrl()) != nil && len(snapshot.GetResourcesAndTTL(request.GetTypeUrl())) == 0)
 	}
 
 	if delayedResponse {
